6aced769206987a1aadd97d76fcfa957a5b4c0dc
Filidor Wiese <fili@fili.nl>
2018-02-21 12:11:06 +0100

    Adds structured data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,8 @@ module.exports = {
       phone: '+31 (0)6 24 99 32 05',
       title: 'Freelance Front-end Developer',
       description: 'I build pixel-perfect applications for the web, using modern techniques and Javascript frameworks, with an emphasis on performance, usability and search-engine friendliness.',
-      other: `Earlier work includes projects for various companies such as
-        Jachthaven Zuidwesthoek, USVA, Gemeente Drenthe, Internethost.nl,
-        Gemeente Groningen, Buyways Rolls, Bevrijdingsfestival Groningen,
-        Theater Peergroup, Vevida, Noorderzon, Four Corners, Kunstencentrum
-        Groningen, and others`
+      other: `Earlier work includes projects for various companies such as Jachthaven Zuidwesthoek, USVA, Gemeente Drenthe, Internethost.nl, Gemeente Groningen, Buyways Rolls, Bevrijdingsfestival Groningen, Theater Peergroup, Vevida, Noorderzon, Four Corners, Kunstencentrum Groningen, and others`,
+      url: 'https://filidorwiese.nl'
     },
     social: {
       github: 'https://github.com/filidorwiese/',
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,28 @@ import Project from '../components/Project'
 
 export default ({ data }) => {
   const siteMetadata = data.site.siteMetadata
-  const pageTitle = `Resume: ${siteMetadata.bio.name}, ${
-    siteMetadata.bio.title
-  }`
+  const pageTitle = `${siteMetadata.bio.name}, ${siteMetadata.bio.title}`
+  const schemaOrg = {
+    type: 'application/ld+json',
+    innerHTML: JSON.stringify({
+      '@context': 'http://schema.org',
+      '@type': 'Person',
+      'email': `mailto: ${siteMetadata.bio.email}`,
+      'image': 'filidor-wiese.jpg',
+      'jobTitle': siteMetadata.bio.title,
+      'description': siteMetadata.bio.description,
+      'name': siteMetadata.bio.name,
+      'telephone': siteMetadata.bio.phone,
+      'url': siteMetadata.url
+    })
+  }
   const projects = data.allMarkdownRemark.edges
   return (
     <main>
       <Helmet
         title={pageTitle}
         meta={[{ name: 'description', content: siteMetadata.bio.description }]}
+        script={[schemaOrg]}
       />
       <div>
         {projects.map((project, key) => (
@@ -29,6 +42,8 @@ export const query = graphql`
       siteMetadata {
         bio {
           name
+          email
+          phone
           title
           description
         }
@@ -54,3 +69,49 @@ export const query = graphql`
     }
   }
 `
+
+
+// return {
+//   type: 'application/ld+json',
+//   innerHTML: JSON.stringify({
+//     '@context': 'http://schema.org',
+//     '@type': 'Organization',
+//     name: 'Leaseplan',
+//     makesOffer: {
+//       '@type': 'Offer',
+//       businessFunction: 'http://purl.org/goodrelations/v1#LeaseOut',
+//       priceCurrency: carCurrencyCode,
+//       price: carPrice,
+//       priceSpecification: {
+//         '@type': 'priceSpecification',
+//         eligibleQuantity: {
+//           '@type': 'QuantitativeValue',
+//           unitCode: 'MON',
+//           unitText: 'Month',
+//           value: 1
+//         }
+//       },
+//       itemOffered: {
+//         '@type': 'Car',
+//         name: carName,
+//         description: car.get('readMoreText'),
+//         image: {
+//           '@type': 'ImageObject',
+//           width: 812,
+//           height: 540,
+//           url: galleryImageUrl
+//         },
+//         color: selectedColor.get('title'),
+//         vehicleEngine: {
+//           '@type': 'EngineSpecification',
+//           name: car.getIn(['specifications', 'engine'])
+//         },
+//         bodyType: car.getIn(['specifications', 'body']),
+//         fuelType: car.getIn(['specifications', 'fuel']),
+//         fuelConsumption: car.getIn(['specifications', 'fuelConsumption']),
+//         vehicleInteriorType: car.getIn(['specifications', 'interior']),
+//         vehicleSeatingCapacity: car.getIn(['specifications', 'seats']),
+//         vehicleTransmission: car.getIn(['specifications', 'transmission'])
+//       }
+//     }
+//   })
